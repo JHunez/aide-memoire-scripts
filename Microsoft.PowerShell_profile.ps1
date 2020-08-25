9ef3f7628adc71dbdfa4d875cd88185bff59e6b8
Julien Hunez <git@hunez.fr>
2020-08-25 17:56:35 +0200

    add autocompletion + commentaires

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+# Force l'encode des caractères en UTF8 
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
+# Alias 
 set-alias -name "dig" -Value "Resolve-DnsName"
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
